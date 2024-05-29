a375552694df8efd94e1ea6739d3e28f2b66c92d
Aboussejra <Amir.Boussejra@viavisolutions.com>
2024-05-29 16:16:33 +0200

    [models] Implement db query to listAllWorkoutsDb

--- models/models.go
+++ models/models.go
@@ -1,43 +1,34 @@
 package models
 
+import "log"
+
 func AddWorkout(name string, reps int, sets int) error {
 	_, err := db.Exec("INSERT INTO workouts (name, reps, sets) VALUES (?, ?, ?)", name, reps, sets)
 	return err
 }
 
-// TODO: write test dummy DB and do real code
+// TODO: write test dummy DB and do real code, Migrate GORM?
 func ListAllWorkoutsDb() []Workout {
-	// rows, err := db.Query("SELECT * FROM workouts")
-	// log.Fatal(rows)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// An album slice to hold data from returned rows.
-	// var workouts []Workout
-	// for rows.Next() {
-	// 	var workout Workout
-	// 	if err := rows.Scan(&workout.Name); err != nil {
-	// 		return workouts
-	// 	}
-	// }
-	workouts := []Workout{
-		{
-			ID:   1,
-			Name: "Leg Day",
-			Exercises: []Exercise{
-				{ID: 1, Name: "Squat"},
-				{ID: 3, Name: "Lunges"},
-			},
-			DayOfWeek: "Monday",
-		},
-		{
-			ID:   2,
-			Name: "Chest Day",
-			Exercises: []Exercise{
-				{ID: 6, Name: "Chest Fly"},
-			},
-			DayOfWeek: "Wednesday",
-		},
+	// Query to select all workouts
+	rows, err := db.Query("SELECT name FROM workouts")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	// Create a slice to hold the workouts
+	var workouts []Workout
+
+	// Iterate over the rows
+	for rows.Next() {
+		var workout Workout
+		if err := rows.Scan(&workout.Name); err != nil {
+			log.Fatal(err)
+		}
+
+		var exercises []Exercise
+		workout.Exercises = exercises
+		workouts = append(workouts, workout)
 	}
 	return workouts
 }
